ea1371a73fad3d46244775c3a99b58a734058fc2
Ivan Moore <ivan@teamoptimization.com>
2023-12-12 16:24:37 +0000

    delete left-over code

--- src/main/java/com/oocode/BestTimesFinder.java
+++ src/main/java/com/oocode/BestTimesFinder.java
@@ -7,9 +7,5 @@ import java.time.ZonedDateTime;
 import java.util.List;
 
 public interface BestTimesFinder {
-    /*
-    "DATE_GMT","TIME_GMT","SETTLEMENT_DATE","SETTLEMENT_PERIOD","EMBEDDED_WIND_FORECAST","EMBEDDED_WIND_CAPACITY","EMBEDDED_SOLAR_FORECAST","EMBEDDED_SOLAR_CAPACITY"
-    "2023-12-11T00:00:00","11:30","2023-12-11T00:00:00",23,1333,6488,2417,15595
-         */
     List<ZonedDateTime> bestTimes() throws IOException, CsvException;
 }
