3241ed6bb7e72cff494f378ccd1ef7be0b9e3ba7
Till <Till.Seifert@d-fine.de>
2024-04-24 11:39:40 +0100

    adding boundary case where third forecast is equal. Expects ordering by date first.

--- src/test/java/com/oocode/ExampleUsingInputFileTest.java
+++ src/test/java/com/oocode/ExampleUsingInputFileTest.java
@@ -30,6 +30,12 @@ public class ExampleUsingInputFileTest {
                 Sun, 5 May 2024 12:00:00 GMT
                 Sun, 5 May 2024 12:30:00 GMT
                 Sun, 5 May 2024 13:00:00 GMT""");
+
+        testFileInput("src/test/resources/embedded-forecast_duplicate.csv", """
+                Best times to plug in:
+                Wed, 24 Apr 2024 08:30:00 GMT
+                Wed, 24 Apr 2024 09:00:00 GMT
+                Wed, 24 Apr 2024 09:30:00 GMT""");
     }
 
     public void testFileInput(String filename, String expectedOutput) throws IOException, CsvException {
